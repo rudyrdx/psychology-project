a345677f0d16d0e5df50168c7078c69e371a8c4e
rudyrdx <rudyrdx21@gmail.com>
2022-09-08 18:46:59 +0530

    asd

--- test/WinFormsApp1/WinFormsApp1/Form2.cs
+++ test/WinFormsApp1/WinFormsApp1/Form2.cs
@@ -11,6 +11,7 @@ namespace WinFormsApp1
 {
     public partial class Form2 : Form
     {
+        Button[] btns;
         public Form2()
         {
             InitializeComponent();
@@ -33,34 +34,51 @@ namespace WinFormsApp1
             this.FormBorderStyle = FormBorderStyle.None;
 
         }
+        
+        List<int> pattern_1 = new List<int> { 255 - 235, 255 - 165, 255 - 215, 
+            255 - 195, 255 - 245, 255 - 175, 255 - 225, 255 - 205, 0, 255 - 185};
+
+        //pattern 2 reverse of pattern_1
+        List<int> pattern_2 = new List<int> { 255 - 185, 0, 255 - 205, 255 - 225,
+            255 - 175, 255 - 245, 255 - 195, 255 - 215, 255 - 165, 255 - 235 };
+
+        //pattern_3 another pattern of 
         private void Form2_Load(object sender, EventArgs e)
         {
+            
+            
+            this.BackColor = Color.White;
             panel1.Size = this.Size;
-            panel1.Location = new Point(0, 0);
+            panel1.Location = new Point(15, 0);
+            panel1.BackColor = Color.White;
 
             int rows = 2;
             int cols = 5;
-            int width = panel1.Width / cols;
-            int height = panel1.Height / rows;
+            int spacing = 10;
+            int width = (panel1.Width / cols) - spacing;
+            int height = (panel1.Height / rows) - spacing;
             int count = 0;
             int red = 255;
+            btns = new Button[10];
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    PictureBox btn = new PictureBox();
+                    red = 255;
+                    Button btn = new Button();
                     btn.Size = new Size(width, height);
-                    btn.Location = new Point(j * width, i * height);
-                    btn.BorderStyle = BorderStyle.FixedSingle;
+                    btn.Location = new Point(j * width + spacing, i * height + spacing);
                     //each iteration, subtract 15 from red
+                    red -= pattern_1.ElementAt(count);
                     btn.BackColor = Color.FromArgb(255, red, 0, 0);
-                    red -= 15;
+                    btn.Text = pattern_1[count].ToString();
                     panel1.Controls.Add(btn);
                     count++;
+                    btns.Append(btn);
                 }
             }
-            panel1.Hide();
 
+            
         }
     }
 }
---
+++ test/WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+    
+            this.WindowState = FormWindowState.Maximized;
+            this.Size = 
+            this.FormBorderStyle = FormBorderStyle.None;
+            panel1.Size = this.Size;
+            panel1.Location = new Point(0, 0);
+            panel1.BackColor = Color.Red;
+            int rows = 2;
+            int cols = 5;
+            int width = panel1.Width / cols;
+            int height = panel1.Height / rows;
+            int count = 0;
+            int red = 255;
+
+            
+            
+        }
+    }
+}
